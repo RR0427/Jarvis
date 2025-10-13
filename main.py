4f690f0a8f6a6cac8bbbe8d2b2acc9c6a8c63b52
Rajesh <rairajesh2552@gmail.com>
2025-10-13 20:45:51 +0100

    added voice commands for news songss and webbrowser

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
+import musicLibrary
+import requests
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
+newsapi = "dc38d352f8364b1e9634dd7e88a2b783"
 
 def speak(text):
+    print(f"Jarvis: {text}")
     engine.say(text)
     engine.runAndWait()
 
@@ -20,31 +24,48 @@ def processCommand(c):
         webbrowser.open("https://instagram.com")
     elif "open twitter" in c.lower():
         webbrowser.open("https://x.com")
+
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        link = musicLibrary.music[song]
+        webbrowser.open(link)
+
+    elif "news" in c.lower():
+        r =requests.get(f"https://newsapi.org/v2/top-headlines?country=us&apiKey={newsapi}")    
+        if r.status_code == 200:
+            data = r.json()  # Parse JSON response
+            articles = data.get('articles', []) # Get the list of articles
+            
+            # Loop through articles and print headlines
+            for article in articles:
+                speak(article['title'])
     
 
 if __name__ == "__main__":
     speak(" Initalizing Jarvis ")
+
     while(True):
                     # Obtain audio from the microphone         
 
-        r = sr.Recognizer()
-
-        print("Recongnizing....")
         try:
             with sr.Microphone() as source:
+                recognizer.adjust_for_ambient_noise(source, duration=1)
+
                 print("Listening....")
-                audio = r.listen(source)
-            command = r.recognize_google(audio)
-            if (command.lower() == "jarvis"):
-                speak("Yes, How may i help you ?")
+                audio = recognizer.listen(source, timeout=6)
+            word = recognizer.recognize_google(audio)
+            print(f"Heard: {word}")
+            if (word.lower() == "jarvis"):
+                speak("yes")
 
                 # Listen for commant
                 with sr.Microphone() as source:
                     print("jarvis Listening.....")
-                    audio = r.listen(source)
-                    command = r.recognize_google(audio)
-
-            processCommand(command)
+                    recognizer.adjust_for_ambient_noise(source, duration=0.5)
+                    audio = recognizer.listen(source, timeout=6, phrase_time_limit=4)
+                    command = recognizer.recognize_google(audio)
+                    print(f"Command: {command}")
+                    processCommand(command)
 
         except Exception as e:
             print(e)
---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "sad": "https://www.youtube.com/watch?v=jS-lpqprf9g",
+    "pain": "https://www.youtube.com/watch?v=Qbz_bsIXo-Y",
+    "naruto": "https://www.youtube.com/watch?v=Qbz_bsIXo-Y",
+    "itachi": "https://www.youtube.com/watch?v=SkRmLo2UGYM"
+
+
+}
